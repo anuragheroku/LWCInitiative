b90a578862a174973d8d6e7aa34da3284091ed28
anuragchawla@outlook.com <anuragchawla@Anurags-MacBook-Pro.local>
2019-07-16 12:50:57 +0200

    added data to hello world

--- LWCBasics/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWCBasics/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,8 +1,23 @@
-import { LightningElement, track } from 'lwc';
+
+import { LightningElement, track, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+const fields = [NAME_FIELD];
+
+
 export default class HelloWorld extends LightningElement {
     @track greeting = 'World';
     changeHandler(event) {
         this.greeting = event.target.value;
+
     }
+    userId = Id;
+    @wire(getRecord, { recordId: '$userId', fields }) 
+    user;
+    get name() {
+        return getFieldValue(this.user.data, NAME_FIELD);
+    }
+
 }
 
