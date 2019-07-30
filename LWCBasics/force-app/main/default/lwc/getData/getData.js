d4efb4e54ce41e4209d71d4b10ac51370bba67aa
Gupta <550374@cognizant.com>
2019-07-30 14:53:15 +0200

    'updateMethod'

--- LWCBasics/force-app/main/default/lwc/getData/getData.js
+++ LWCBasics/force-app/main/default/lwc/getData/getData.js
@@ -1,7 +1,11 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 //import { getListUi } from 'lightning/uiListApi';
 //import ResourcePool_obj from '@salesforce/schema/Resource_Pool__c';
-import getAllResources from '@salesforce/apex/getResourceList.getAllResources'
+import { updateRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import getAllResources from '@salesforce/apex/getResourceList.getAllResources';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 const actions = [
     { label: 'Show details', name: 'show_details' },
@@ -12,8 +16,7 @@ const actions = [
 const columns = [
     { label: 'Name', fieldName: 'Name', type: 'text' },
     { label: 'Type', fieldName: 'Type__c', type: 'text' },
-    { label: 'Prospective Account', fieldName: 'Account__c', type: 'text' },
-    { label: 'Opprtunity', fieldName: 'Opportunity__c', type: 'text' },
+    { label: 'isAvailable', fieldName: 'isAvailable__c', type: 'checkbox' },
     {
         type: 'action',
         typeAttributes: { rowActions: actions },
@@ -22,7 +25,72 @@ const columns = [
 
 export default class Example extends LightningElement {
     @track columns = columns;
-
+    @track resources;
     @wire(getAllResources)
     resources;
+    @api recordId;
+    @api objectApiName;
+
+handleRowAction(event) {
+    const actionName = event.detail.action.name; 
+    const row = event.detail.row;
+    // eslint-disable-next-line no-console
+    console.log('GGUUMyHHHHey its the event detail row'+row);
+    switch (actionName) {
+        case 'delete':
+            this.deleteRow(row);
+            break;
+        case 'show_details':
+            this.showRowDetails(row);
+            break;
+        case 'assign':
+        this.updateMe(row);
+        break;
+        default:
+    }
+}
+
+updateMe(row)
+{
+
+    console.log('Getting ROW 8');
+    console.log(JSON.stringify(row));
+
+    const fields= {} ;
+    // eslint-disable-next-line guard-for-in
+    for(var property in row) {
+         fields[property]=row[property];
+       // alert(property + "=" + row[property]);
+
+   }
+
+    fields.Opportunity__c=this.recordId;
+    fields.isAvailable__c = true;
+    
+    console.log('Below is prepared fields object');
+    console.log(JSON.stringify(fields));
+    const recordInput = {fields};
+    updateRecord(recordInput)
+                .then(() => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message: 'Record updated',
+                            variant: 'success'
+                        })                        
+                    );
+                   return refreshApex(this.resources);
+                    
+                })
+                .catch(error => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Error creating record',
+                            message: error.body.message,
+                            variant: 'error'
+                        })
+                    );
+                });
+
+}
 }
